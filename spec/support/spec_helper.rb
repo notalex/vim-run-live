77f0f6d59094e2c362d2fb730574d28437458152
Alex Johnson <notalexjohnson@gmail.com>
2013-05-04 19:27:25 +0530

    tests: setting up vimrunner with minitest

---
+++ spec/support/spec_helper.rb
@@ -0,0 +1,33 @@
+require 'tmpdir'
+require 'vimrunner'
+require 'vimrunner/testing'
+require 'minitest/autorun'
+
+include Vimrunner::Testing
+
+def vim
+  @vim ||= Vimrunner.start
+end
+
+class MiniTest::Spec
+  before do
+    vim.command("cd #{ tmp_dir_path }")
+    plugin_path = File.expand_path('../../../', __FILE__)
+    vim.add_plugin(plugin_path, 'plugin/run_live.vim')
+  end
+
+  after do
+    remove_tmp_directory
+    vim.kill
+  end
+
+  private
+
+  def tmp_dir_path
+    @tmp_dir_path ||= Dir.mktmpdir('vimrunner')
+  end
+
+  def remove_tmp_directory
+    FileUtils.remove_entry(tmp_dir_path)
+  end
+end
